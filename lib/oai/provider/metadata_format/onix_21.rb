a0e05fcbdbba4e1dee897e46c3e6629a2c78c5a9
Josyan Pierson <josyan@demarque.com>
2011-06-21 09:10:29 -0400

    Added Onix 2.1 format.

---
+++ lib/oai/provider/metadata_format/onix_21.rb
@@ -0,0 +1,33 @@
+module OAI::Provider::Metadata
+  # = OAI::Metadata::Onix21
+  #
+  # Simple implementation of the Dublin Core metadata format
+  # plus output in Onix 2.1 format
+  class Onix21 < Format
+
+    def initialize
+      @prefix = 'onix_dc'
+      @schema = 'http://bibnum.bnf.fr/NS/onix_dc.xsd'
+      @namespace = 'http://bibnum.bnf.fr/NS/onix_dc/'
+      @element_namespace = 'dc'
+      @fields = [ :title, :creator, :subject, :description, :publisher,
+                  :contributor, :date, :type, :format, :identifier,
+                  :source, :language, :relation, :coverage, :rights ]
+    end
+
+    def header_specification
+      {
+        'xmlns:onix_dc' => "http://bibnum.bnf.fr/NS/onix_dc/",
+        'xmlns:dc' => "http://purl.org/dc/elements/1.1/",
+        'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
+        'xmlns:onix' => "http://www.editeur.org/onix/2.1/reference",
+        'xsi:schemaLocation' =>
+          %{http://bibnum.bnf.fr/NS/onix_dc/
+            http://bibnum.bnf.fr/NS/onix_dc.xsd}
+      }
+    end
+
+  end
+end
+
+
